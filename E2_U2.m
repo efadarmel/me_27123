084ff13ddfda30e578b43b1143c706176e51f807
Edinson Fabian Adarme <130710995+efadarmel@users.noreply.github.com>
2026-04-21 13:30:12 -0500

    Update E2_U2.m

--- E2_U2.m
+++ E2_U2.m
@@ -18,3 +18,52 @@ clear      % Borra todas las variables del workspace
 % se obtienen los siguientes vectores de muestras:
 vs = [-130.900 -168.291 -107.100 -16.829 130.900 168.291 107.100 16.829 -130.900 -168.291];
 is = [-0.776 -6.061 -8.479 -5.558 0.776 6.061 8.479 5.558 -0.776 -6.061];
+
+% --- Parámetros de muestreo ---
+Fs = 480;      % Frecuencia de muestreo [Hz]
+fo = 60;       % Frecuencia fundamental de la red [Hz]
+nSpC = Fs/fo;  % Número de muestras por ciclo
+N = nSpC;      % Número de muestras de la ventana de observación
+
+% --- Selección de una ventana de tensión y corriente ---
+vs = vs(1:N);   
+is = is(1:N);   
+
+% --- Cálculo de resistencias de línea y neutro ---
+rl = 5.47*20/1000;   % Resistencia del conductor de línea (Ω)
+rn = 2.15*20/1000;   % Resistencia del conductor de neutro (Ω)
+
+% --- Tensión en la carga por LVK ---
+vL = vs - rl.*is - rn.*is;
+
+% --- Valores eficaces ---
+VLrms = sqrt((1/nSpC)*sum(vL.^2));   % Tensión eficaz en la carga
+Vsrms = sqrt((1/nSpC)*sum(vs.^2));   % Tensión eficaz en el alimentador
+Isrms = sqrt((1/nSpC)*sum(is.^2));   % Corriente eficaz
+
+% --- Modelo de Fryze el alimentador ---
+psn = vs.*is;              % Potencia instantánea 
+Ps = (1/N)*sum(psn);       % Valor medio de p[n] → Potencia activa
+Ss = Vsrms*Isrms;          % Potencia aparente
+Qfs = sqrt(Ss^2 - Ps^2);   % Potencia reactiva de Fryze
+fps = Ps/Ss;               % Factor de potencia
+
+% --- Modelo de Budeanu en el alimentador ---
+iBs = [is(3:8), is(1:2)];   % Corriente de Budeanu
+QBs = (1/N)*sum(vs.*iBs);           % Potencia reactiva Budeanu
+DBs = sqrt(Ss^2 - Ps^2 - QBs^2);    % Potencia de Distorsión de Budeanu
+
+% --- Modelo de Fryze en la carga ---
+pLn = vL.*is;              % Potencia instantánea en la carga
+PL = (1/N)*sum(pLn);       % Valor medio de p[n] → Potencia activa
+SL = VLrms*Isrms;          % Potencia aparente en la carga
+QfL = sqrt(SL^2 - PL^2);   % Potencia reactiva de Fryze en la carga
+fpL = PL/SL;               % Factor de potencia de la carga
+
+QBL = (1/N)*sum(vL.*iBs);          % Potencia reactiva Budeanu en la carga
+DBL = sqrt(SL^2 - PL^2 - QBL^2);   % Potencia de Distorsión de Budeanu en la carga
+
+% --- Tabla de resultados ---
+T = table([Ps;PL],[Ss;SL],[Qfs;QfL],[fps;fpL],[QBs;QBL],[DBs;DBL], ...
+    VariableNames={'P','S','Qf','fp','QB','DB'});
+disp(T)
